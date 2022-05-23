3cd263725e1386afa6aa39e61f8d85be5096192c
oustriX <50222966+oustriX@users.noreply.github.com>
2022-05-23 18:57:08 +1000

    release connect to DB

--- pkg/pg/connect.go
+++ pkg/pg/connect.go
@@ -1,1 +1,23 @@
 package pg
+
+import (
+	"context"
+	"errors"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"gopkg.in/ini.v1"
+)
+
+func DbConnect() (*pgxpool.Pool, error) {
+	cfg, err := ini.Load("../../config/config.ini")
+	if err != nil {
+		return nil, errors.New("failed to read config file")
+	}
+
+	connStr := cfg.Section("").Key("DB_CON").String()
+	conn, err := pgxpool.Connect(context.Background(), connStr)
+	if err != nil {
+		return nil, errors.New("failed to connect to DB")
+	}
+
+	return conn, nil
+}
