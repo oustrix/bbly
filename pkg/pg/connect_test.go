fc82e2601e7d0b8ee78a892b37afffc5d3598e2d
oustriX <50222966+oustriX@users.noreply.github.com>
2022-05-26 17:30:10 +1000

    add unit test for getDBConnectionConfig

--- pkg/pg/connect_test.go
+++ pkg/pg/connect_test.go
@@ -1,1 +1,12 @@
 package pg
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetDBConnectionConfig(t *testing.T) {
+	_, err := getDBConnectionConfig("")
+	require.Error(t, err)
+}
