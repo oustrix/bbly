98a6fad2606b60b6585e350eb051adb9885d369f
oustriX <50222966+oustriX@users.noreply.github.com>
2022-05-27 17:02:44 +1000

    improve error handling

--- internal/handlers/redirect.go
+++ internal/handlers/redirect.go
@@ -16,9 +16,10 @@ func Redirect(c *gin.Context) {
 	var url string
 	err := row.Scan(&url)
 	if err != nil {
-		log.Fatal("problems to select url with id" + id)
+		log.Println("problems to select url with id" + id)
+		c.HTML(http.StatusInternalServerError, "server_error.html", gin.H{})
+	} else {
+		c.Redirect(http.StatusMovedPermanently, url)
+		c.Abort()
 	}
-	log.Printf("found url %s by id %s\n", url, id)
-	c.Redirect(http.StatusMovedPermanently, url)
-	c.Abort()
 }
