954597ed9a2c6563779322bde1dbd4237da3d015
oustriX <50222966+oustriX@users.noreply.github.com>
2022-05-27 17:56:00 +1000

    update Redirect function

--- internal/handlers/redirect.go
+++ internal/handlers/redirect.go
@@ -10,25 +10,41 @@ import (
 	"bbly/pkg/pg"
 )
 
+type Row struct {
+	id     string
+	url    string
+	visits int
+}
+
 func Redirect(c *gin.Context) {
-	id := getID(c)
-	url, err := searchURL(id)
+	var row Row
+	var err error
+
+	row.id = getID(c)
+	row.url, row.visits, err = searchURLAndVisits(row.id)
 	if err != nil {
-		log.Println("problems to select url with id" + id)
+		log.Println("problems to select url with id" + row.id)
 		c.HTML(http.StatusInternalServerError, "server_error.html", gin.H{})
 	} else {
-		c.Redirect(http.StatusMovedPermanently, url)
+		_, err := pg.DB.Exec(context.Background(), "UPDATE links SET visits=$1 WHERE id=$2", row.visits+1, row.id)
+		if err != nil {
+			log.Printf("failed to increment visits counter for %s\n", row.id)
+		}
+		c.Redirect(http.StatusMovedPermanently, row.url)
 		c.Abort()
 	}
 }
 
+// get id from request
 func getID(c *gin.Context) string {
 	return c.Param("id")
 }
 
-func searchURL(id string) (string, error) {
-	row := pg.DB.QueryRow(context.Background(), "SELECT url FROM links WHERE id=$1 LIMIT 1;", id)
+// search for full URL and visits from DB by id
+func searchURLAndVisits(id string) (string, int, error) {
+	row := pg.DB.QueryRow(context.Background(), "SELECT url, visits FROM links WHERE id=$1 LIMIT 1;", id)
 	var url string
-	err := row.Scan(&url)
-	return url, err
+	var visits int
+	err := row.Scan(&url, &visits)
+	return url, visits, err
 }
