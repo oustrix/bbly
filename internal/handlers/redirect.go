6d9b9243b89b4beb47107b3761cb2d1fff55320f
oustriX <50222966+oustriX@users.noreply.github.com>
2022-05-27 17:00:35 +1000

    add redirect function

--- internal/handlers/redirect.go
+++ internal/handlers/redirect.go
@@ -1,9 +1,24 @@
 package handlers
 
 import (
+	"context"
+	"log"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+
+	"bbly/pkg/pg"
 )
 
 func Redirect(c *gin.Context) {
-
+	id := c.Param("id")
+	row := pg.DB.QueryRow(context.Background(), "SELECT url FROM links WHERE id=$1 LIMIT 1;", id)
+	var url string
+	err := row.Scan(&url)
+	if err != nil {
+		log.Fatal("problems to select url with id" + id)
+	}
+	log.Printf("found url %s by id %s\n", url, id)
+	c.Redirect(http.StatusMovedPermanently, url)
+	c.Abort()
 }
