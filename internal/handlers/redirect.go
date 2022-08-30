0102d4f173af57825207f1b608559494ee1837cb
oustriX <50222966+oustriX@users.noreply.github.com>
2022-08-30 23:50:56 +0300

    some refactoring

--- internal/handlers/redirect.go
+++ internal/handlers/redirect.go
@@ -26,7 +26,7 @@ func Redirect(c *gin.Context) {
 	row.id = getID(c)
 	row.url, row.visits, err = searchURLAndVisits(row.id)
 	if err != nil {
-		log.Println("problems to select url with id" + row.id)
+		log.Println("problems to select url with id=" + row.id)
 		c.HTML(http.StatusInternalServerError, "server_error.html", gin.H{})
 	} else {
 		_, err := pg.DB.Exec(context.Background(), "UPDATE links SET visits=$1 WHERE id=$2", row.visits+1, row.id)
@@ -39,15 +39,13 @@ func Redirect(c *gin.Context) {
 }
 
 // get id from request
-func getID(c *gin.Context) string {
+func getID(c *gin.Context) (id string) {
 	return c.Param("id")
 }
 
 // search for full URL and visits from DB by id
-func searchURLAndVisits(id string) (string, int, error) {
+func searchURLAndVisits(id string) (url string, visits int, err error) {
 	row := pg.DB.QueryRow(context.Background(), "SELECT url, visits FROM links WHERE id=$1 LIMIT 1;", id)
-	var url string
-	var visits int
-	err := row.Scan(&url, &visits)
+	err = row.Scan(&url, &visits)
 	return url, visits, err
 }
